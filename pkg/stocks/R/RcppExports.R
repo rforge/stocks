f79c8e010695f53ee7361be5d57c0f644761786b
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-19 19:51:34 +0000

    Added pchanges_c C++ function and pchanges R function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stocks@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stocks/R/RcppExports.R
+++ pkg/stocks/R/RcppExports.R
@@ -18,6 +18,22 @@ pdiffs <- function(x, lag = 1, percent = FALSE) {
 
 }
 
+pchanges <- function(x, lag = 1, percent = FALSE) {
+  
+  # Check that percent is a logical
+  if (!is.logical(percent)) {
+    stop("For percent input, please enter TRUE or FALSE")
+  }
+  
+  # Call pchanges_c with multiplier depending on percent input
+  if (percent) {
+    .Call('stocks_pchanges_c', PACKAGE = 'stocks', x, lag, 100)
+  } else {
+    .Call('stocks_pchanges_c', PACKAGE = 'stocks', x, lag, 1)
+  }
+  
+}
+
 convert.rate <- function(rate, days.in = 1, days.out = 1) {
   out.rate <- ((rate + 1)^(days.out/days.in)) - 1
   return(out.rate)
--- pkg/stocks/src/RcppExports.cpp
+++ pkg/stocks/src/RcppExports.cpp
@@ -35,3 +35,20 @@ BEGIN_RCPP
     return __sexp_result;
 END_RCPP
 }
+// pchanges_c
+NumericVector pchanges_c(NumericVector x, int lag, double multiplier);
+RcppExport SEXP stocks_pchanges_c(SEXP xSEXP, SEXP lagSEXP, SEXP multiplierSEXP) {
+BEGIN_RCPP
+    SEXP __sexp_result;
+    {
+        Rcpp::RNGScope __rngScope;
+        NumericVector x = Rcpp::as<NumericVector >(xSEXP);
+        int lag = Rcpp::as<int >(lagSEXP);
+        double multiplier = Rcpp::as<double >(multiplierSEXP);
+        NumericVector __result = pchanges_c(x, lag, multiplier);
+        PROTECT(__sexp_result = Rcpp::wrap(__result));
+    }
+    UNPROTECT(1);
+    return __sexp_result;
+END_RCPP
+}
