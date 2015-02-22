9b7fd79ac86eb5758bb43fafb93f2be1a596c27b
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-22 21:27:10 +0000

    Added all functions developed to date. Also a sample dataset zzz with daily closing prices for an imaginary stock.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stocks@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stocks/R/RcppExports.R
+++ pkg/stocks/R/RcppExports.R
@@ -34,20 +34,112 @@ pchanges <- function(x, lag = 1, percent = FALSE) {
   
 }
 
+ratios <- function(x) {
+  .Call('stocks_ratios_c', PACKAGE = 'stocks', x)
+}
+
 convert.rate <- function(rate, days.in = 1, days.out = 1) {
-  out.rate <- ((rate + 1)^(days.out/days.in)) - 1
-  return(out.rate)
+  ((rate + 1)^(days.out/days.in)) - 1
+}
+
+daily.yearly <- function(daily.gain, years = 1) {
+  (1 + daily.gain)^(251*years) - 1
+}
+
+yearly.daily <- function(total.gain, years = 1) {
+  (total.gain + 1)^(1/(251*years)) - 1
 }
 
 balances <- function(ratios, initial = 10000) {
   c(initial, initial * cumprod(ratios))
 }
 
-mdd <- function(prices = NULL, highs = NULL, lows = NULL, indices = FALSE) {
+final.balance <- function(ratios, initial = 10000) {
+  initial * prod(ratios)
+}
+
+prices.rate <- function(prices, xday.rate = NULL) {
+  
+  # Get the overall growth rate
+  prices.length <- length(prices)
+  rate1 <- prices[prices.length] / prices[1] - 1
+  
+  # Convert to x-day growth rate if xday.rate is specified
+  if (! is.null(xday.rate) && xday.rate != prices.length - 1) {
+    rate1 <- convert.rate(rate = rate1, days.in = prices.length - 1, days.out = xday.rate)
+  }
+  
+  # Return the rate
+  return(rate1)
+  
+}
+
+gains.rate <- function(gains, xday.rate = NULL) {
+  
+  # Get the overall growth rate
+  gains.length <- length(gains)
+  rate1 <- prod(gains + 1) - 1
+  
+  # Convert to x-day growth rate if xday.rate is specified
+  if (! is.null(xday.rate) && ! (xday.rate == gains.length)) {
+    rate1 <- convert.rate(rate = rate1, days.in = gains.length, days.out = xday.rate)
+  }
+  
+  # Return the rate
+  return(rate1)
+  
+}
+
+positives <- function(x, include.zero = FALSE) {
+  
+  # Check that include.zero is a logical
+  if (!is.logical(include.zero)) {
+    stop("For include.zero input, please enter TRUE or FALSE")
+  }
+  
+  # Get positive values
+  if (include.zero) {
+    x[which(x >= 0)]
+  } else {
+    x[which(x > 0)]
+  }
+  
+}
+
+negatives <- function(x, include.zero = FALSE) {
+  
+  # Check that include.zero is a logical
+  if (!is.logical(include.zero)) {
+    stop("For include.zero input, please enter TRUE or FALSE")
+  }
+  
+  # Get negative values
+  if (include.zero) {
+    x[which(x <- 0)]
+  } else {
+    x[which(x < 0)]
+  }
+  
+}
+
+nonpositives <- function(x) {
+  x[which(x <= 0)]
+}
+
+nonnegatives <- function(x) {
+  x[which(x >= 0)]
+}
+
+mdd <- function(prices = NULL, gains = NULL, highs = NULL, lows = NULL, indices = FALSE) {
   
   # Check that indices is a logical
   if (!is.logical(indices)) {
     stop("For indices input, please enter TRUE or FALSE")
+  }
+  
+  # If gains specified rather than prices, convert to prices
+  if (!is.null(gains)) {
+    prices <- balances(ratios = gains + 1, initial = 1)
   }
   
   # Call C++ function depending on indices and whether prices or highs and lows specified
@@ -60,7 +152,6 @@ mdd <- function(prices = NULL, highs = NULL, lows = NULL, indices = FALSE) {
       names(mdd.out) <- c("mdd", "start.index", "end.index")
     } else {
       mdd.out <- .Call('stocks_mdd_p_c1', PACKAGE = 'stocks', prices)
-      names(mdd.out) <- "mdd"
     }
   } else {
     if (!is.null(prices)) {
@@ -71,7 +162,6 @@ mdd <- function(prices = NULL, highs = NULL, lows = NULL, indices = FALSE) {
       names(mdd.out) <- c("mdd", "start.index", "end.index")
     } else {
       mdd.out <- .Call('stocks_mdd_hl_c1', PACKAGE = 'stocks', highs, lows)
-      names(mdd.out) <- "mdd"
     }
   }
     
@@ -80,6 +170,47 @@ mdd <- function(prices = NULL, highs = NULL, lows = NULL, indices = FALSE) {
   
 }
 
-ratios <- function(x) {
-  .Call('stocks_ratios_c', PACKAGE = 'stocks', x)
+sharpe <- function(gains = NULL, prices = NULL, rf = 0) {
+  
+  # Convert from prices to gains if necessary
+  if (!is.null(prices)) {
+    gains <- pchanges_c(prices, 1, 1)
+  }
+  
+  # Calculate Sharpe ratio
+  (mean(gains) - rf) / sd(gains)
+  
+}
+
+sortino <- function(gains = NULL, prices = NULL, rf = 0) {
+  
+  # Convert from prices to gains if necessary
+  if (!is.null(prices)) {
+    gains <- pchanges_c(prices, 1, 1)
+  }
+  
+  # Calculate Sortino ratio
+  (mean(gains) - rf) / sd(negatives(gains))
+  
+}
+
+rrr <- function(prices = NULL, gains = NULL) {
+  
+  # Check that either prices or gains is specified
+  if (is.null(prices) & is.null(gains)) {
+    stop("Please enter a prices vector or a gains vector")
+  }
+  
+  # Calculate overall growth rate
+  if (!is.null(prices)) {
+    ret <- prices.rate(prices)
+    max.dd <- mdd(prices = prices)
+  } else {
+    ret <- gains.rate(gains)
+    max.dd <- mdd(gains = gains)
+  }
+  
+  # Calculate risk-return ratio
+  ret / max.dd
+  
 }
