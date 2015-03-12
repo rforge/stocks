ddb82b9dfaad51602bd946e2fd25d85a1f040d16
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-12 05:02:15 +0000

    Added capm.ticker function, and dependence on quantmod package since this function uses it. May get warnings/notes due to function returning warnings from calls to getSymbols in quantmod.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stocks@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stocks/R/RcppExports.R
+++ pkg/stocks/R/RcppExports.R
@@ -224,3 +224,99 @@ rrr <- function(prices = NULL, gains = NULL) {
   ret / max.dd
   
 }
+
+capm.ticker <- function(ticker, index.ticker = "^GSPC", from = "1950-01-03", to = Sys.Date(), interval.days = 1, 
+                        decimals = getOption("digits"), gains = FALSE) {
+  
+  # Check that inputs are valid
+  if (! is.character("ticker")) {
+    stop("For ticker input, please enter a character string like 'AAPL' for Apple.")
+  }
+  if (! is.character(index.ticker)) {
+    stop("For index.ticker input, please enter a character string like '^GSPC' for the S&P 500.")
+  }
+  if (! class(from) %in% c("character", "Date")) {
+    stop("For from input, please enter a date or a character string that looks like a date (e.g. '1950-01-03' for January 3, 1950).")
+  }
+  if (! class(to) %in% c("character", "Date")) {
+    stop("For to input, please enter a date or a character string that looks like a date (e.g. '2015-03-09' for March 9, 2015).")
+  }
+  if (interval.days < 1 | ! floor(interval.days) == interval.days) {
+    stop("For interval.days input, please enter a whole number greater than 0.")
+  }
+  if (decimals < 0 | ! floor(decimals) == decimals) {
+    stop("For decimals input, please enter a whole number greater than or equal to 0.")
+  }
+  
+  # Download stock prices for ticker
+  ticker.prices <- as.matrix(getSymbols(Symbols = ticker, from = from, to = to, auto.assign = FALSE, warnings = FALSE))
+  ticker.dates <- rownames(ticker.prices)
+  
+  # Adjust from and to to ensure that index loads in right
+  from <- rownames(ticker.prices)[1]
+  to <- rownames(ticker.prices)[length(rownames(ticker.prices))]
+  
+  # Download stock prices for index
+  index.prices <- as.matrix(getSymbols(Symbols = index.ticker, from = from, to = to, auto.assign = FALSE, warnings = FALSE))
+  index.dates <- rownames(index.prices)
+  
+  # Check that dates match up
+  if (! all(rownames(ticker.prices) == rownames(index.prices))) {
+    
+    fullmatch <- FALSE
+    while (fullmatch == FALSE) {
+      
+      mismatches <- which(ticker.dates != index.dates)
+      if (length(mismatches) == 0) {
+        fullmatch <- TRUE
+      } else {
+        first.mismatch <- mismatches[1]
+        if (length(first.mismatch) == 1) {
+          if (as.Date(ticker.dates[first.mismatch]) > as.Date(index.dates[first.mismatch])) {
+            index.prices <- index.prices[-first.mismatch, ]
+            index.dates <- index.dates[-first.mismatch]
+          } else {
+            ticker.prices <- ticker.prices[-first.mismatch, ]
+            ticker.dates <- ticker.dates[-first.mismatch]
+          }
+        } else {
+          fullmatch <- TRUE
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  # Calculate gains
+  if (interval.days > 1) {
+    
+    ticker.prices <- ticker.prices[seq(1, length(ticker.dates), interval.days), ]
+    index.prices <- index.prices[seq(1, length(ticker.dates), interval.days), ]
+    
+  }
+  
+  # Calculate gains
+  ticker.gains <- pchanges(x = ticker.prices[, 6])
+  index.gains <- pchanges(x = index.prices[, 6])
+  
+  # Create return list with from date, to date, alpha, beta, alpha.p, beta.p, and r2
+  summary.fit <- summary(lm(ticker.gains ~ index.gains))
+  ret <- list(from = from,
+              to = to,
+              alpha = round(summary.fit$coef[1, 1], decimals),
+              beta = round(summary.fit$coef[2, 1], decimals),
+              alpha.p = round(summary.fit$coef[1, 4], decimals),
+              beta.p = round(summary.fit$coef[2, 4], decimals),
+              r2 = round(summary.fit$r.squared, decimals))
+  
+  if (gains == TRUE) {
+    ret$ticker.gains <- ticker.gains
+    ret$index.gains <- index.gains
+  }
+  
+  # Return object
+  return(ret)
+  
+}
