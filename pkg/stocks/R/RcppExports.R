632851969ab8ead860b34c717c9f677654d8180f
vandomed <vandomed@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-10-20 15:51:18 +0000

    Added targetall.nfunds function and changed title of package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stocks@105 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stocks/R/RcppExports.R
+++ pkg/stocks/R/RcppExports.R
@@ -6579,6 +6579,96 @@ targetbeta.twofunds <- function(tickers = NULL,
 }
 
 
+targetall.nfunds <- function(tickers = NULL,
+                             intercepts = NULL, slopes = NULL, ...,
+                             tickers.gains = NULL,
+                             target.alls = NULL,
+                             tol = 0.05,
+                             rebalance.cost = 0,
+                             initial = 10000) {
+
+  # If tickers specified, load various historical prices from Yahoo! Finance
+  if (! is.null(tickers)) {
+
+    # Get number of tickers
+    n.tickers <- length(tickers)
+
+    # If unspecified, use equal target allocations
+    if (is.null(target.alls)) {
+      target.alls <- rep(1 / n.tickers, n.tickers)
+    }
+
+    # If intercepts or slopes NULL, set to matrix of 0's and 1's, respectively
+    if (is.null(intercepts)) {
+      intercepts <- rep(0, n.tickers)
+    }
+    if (is.null(slopes)) {
+      slopes <- rep(1, n.tickers)
+    }
+
+    # Calculate tickers.gains matrix
+    tickers.gains <- load.gains(tickers = tickers, intercepts = intercepts,
+                                slopes = slopes, ...)
+
+    # Update ticker names to show intercept/slope
+    tickers <- colnames(tickers.gains)[1: n.tickers]
+
+  } else {
+
+    # Get number of tickers
+    n.tickers <- ncol(tickers.gains)
+
+    # Figure out tickers from tickers.gains
+    tickers <- colnames(tickers.gains)
+    if (is.null(tickers)) {
+      tickers <- paste("FUND", 1: n.tickers, sep = "")
+    }
+
+  }
+
+  # Create tickers.ratios matrix
+  tickers.ratios <- tickers.gains + 1
+
+  # Initialize variables used in looping through daily gains
+  fund.balances <- matrix(NA, ncol = n.tickers + 1, nrow = nrow(tickers.gains) + 1)
+  rebalance.count <- 0
+
+  # Calculate initial balances in each fund
+  port.bal <- initial
+  funds.bal <- port.bal * target.alls
+  fund.balances[1, ] <- c(funds.bal, port.bal)
+
+  # Loop through daily gains and update portfolio and fund balances
+  for (ii in 1: nrow(tickers.gains)) {
+
+    # Update balances
+    funds.bal <- funds.bal * tickers.ratios[ii, ]
+    port.bal <- sum(funds.bal)
+    fund.balances[(ii + 1), ] <- c(funds.bal, port.bal)
+
+    # Check fund allocations
+    funds.all <- funds.bal / port.bal
+
+    # Rebalance if necessary
+    if (any(abs(funds.all - target.alls) > tol) & ii < nrow(tickers.gains)) {
+      rebalance.count <- rebalance.count + 1
+      port.bal <- port.bal - rebalance.cost
+      funds.bal <- port.bal * target.alls
+    }
+
+  }
+
+  # Add column names to fund.balances
+  colnames(fund.balances) <- c(tickers, "Portfolio")
+
+  # Return list containing fund.balances and rebalance.count
+  results.list <- list(fund.balances = fund.balances,
+                       rebalance.count = rebalance.count)
+  return(results.list)
+
+}
+
+
 # Contango-based XIV/VXX strategy. Hold XIV when contango > c1, hold VXX when
 # contango < c2
 contango.simple <- function(contango,
